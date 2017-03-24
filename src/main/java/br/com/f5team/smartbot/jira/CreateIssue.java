b709437c85df6aaf4626c6e0b0a973a9a2c7a90b
Marcos <marcos.cristiano.af@gmail.com>
2017-03-24 13:29:12 -0300

    Criação jira via Rest - Jersey

--- src/main/java/br/com/f5team/smartbot/jira/CreateIssue.java
+++ src/main/java/br/com/f5team/smartbot/jira/CreateIssue.java
@@ -10,16 +10,49 @@ import java.net.URLEncoder;
 
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.security.sasl.AuthenticationException;
 
 import org.apache.commons.io.IOUtils;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 import com.sun.org.apache.xml.internal.security.utils.Base64;
  
 public class CreateIssue {
     public static void main(String[] args) {
     	
-    	String url = "https://jira.cpqd.com.br/rest/servicedeskapi/request";
+    	String url = "https://jira.cpqd.com.br/rest/api/2/issue/";
+    	
+    	//https://jira.cpqd.com.br/rest/api/2/issue/createmeta?projectKeys=HACK&issuetypeNames=Incidente&expand=projects.issuetypes.fields
+    	//String url = "https://jira.cpqd.com.br/rest/servicedeskapi/request";
         try {
+        	
+        	
+        	
+        	
+    		
+    		
+    		String auth = new String(Base64.encode("marcosaf:;%25lindEN".getBytes(), 0));
+    		
+    		Client client = Client.create();
+    		WebResource webResource = client.resource("https://jira.cpqd.com.br/rest/api/2/issue/");
+    		
+    		ClientResponse response = webResource.header("Authorization", "Basic " + auth).type("application/json").accept("application/json").get(ClientResponse.class);
+    		
+    		int statusCode = response.getStatus();
+    		if (statusCode == 401) {
+    		    throw new AuthenticationException("Invalid Username or Password");
+    		}
+    		
+    		
+    		
+    		String response = response.getEntity(String.class);
+    		
+    		
+        	
+        	
+        	
             URL jiraREST_URL = new URL(url);
             URLConnection urlConnection = jiraREST_URL.openConnection();
             urlConnection.setDoInput(true);
@@ -32,7 +65,7 @@ public class CreateIssue {
  
             System.out.println(getJSonBody() + "/" + encodedData);
             conn.setRequestMethod("POST");
-            conn.setRequestProperty("Authorization", "Basic " + Base64.encode("marcosaf:SENHA".getBytes(), 0));
+            conn.setRequestProperty("Authorization", "Basic " + Base64.encode("marcosaf:;%25lindEN".getBytes(), 0));
             conn.setRequestProperty("Content-Type", "application/json");
             conn.setRequestProperty("Content-Length", String.valueOf(encodedData.length()));
             conn.getOutputStream().write(encodedData.getBytes());
@@ -61,27 +94,38 @@ public class CreateIssue {
   
     	
     	
-    	JsonObject createIssueJira = Json.createObjectBuilder()
-                .add("fields", Json.createObjectBuilder()
+    	JsonObject createIssueJiraHelpDesk = Json.createObjectBuilder()
         		.add("serviceDeskId", "34")
         		.add("requestTypeId", "158")
         		.add("requestFieldValues",Json.createObjectBuilder()
         				.add("summary","Criando jira via REST - F5Team")
         				.add("description", "Criando jira via REST - F5Team"))
-                ).build();
+                .build();
     	
     	
+//    	{
+//    	    "fields": {
+//    	       "project":
+//    	       { 
+//    	          "key": "HACK"
+//    	       },
+//    	       "summary": "REST ye merry gentlemen.",
+//    	       "description": "Creating of an issue using project keys and issue type names using the REST API",
+//    	       "issuetype": {
+//    	          "name": "Bug"
+//    	       }
+//    	   }
+//    	}
+
     	
     	
-    	
-        JsonObject createIssue = Json.createObjectBuilder()
+        JsonObject createIssueJira
+        = Json.createObjectBuilder()
                 .add("fields",Json.createObjectBuilder()
-        		.add("serviceDeskId", "34")
-        		.add("requestTypeId", "158")
-        		.add("project",Json.createObjectBuilder().add("key","HACK-4"))
-                .add("summary", "Usuário não consegue executar operação XPTO")
-                .add("description", "Usuário não consegue executar operação XPTO")
-                .add("issuetype", Json.createObjectBuilder().add("id", "1"))
+        		.add("project",Json.createObjectBuilder().add("key","HACK"))
+                .add("summary", "TESTE JIRA")
+                .add("description", "TESTE JIRA")
+                .add("issuetype", Json.createObjectBuilder().add("name", "Incidente"))
                 ).build();
  
         return createIssueJira.toString();
---
+++ src/main/java/br/com/f5team/smartbot/rest/JTricksRESTClient.java
@@ -0,0 +1,126 @@
+package br.com.f5team.smartbot.rest;
+
+
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.naming.AuthenticationException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.Base64;
+
+public class JTricksRESTClient {
+	
+	private static final String BASE_URL = "https://jira.cpqd.com.br";
+	
+	public void createJiraIssue(String summary, String description, String issueType){
+		String auth = new String(Base64.encode("marcosaf:SENHA"));
+		
+		try {
+//			//Get Projects
+//			String projects = invokeGetMethod(auth, BASE_URL+"/rest/api/2/project");
+//			System.out.println(projects);
+//			JSONArray projectArray = new JSONArray(projects);
+//			for (int i = 0; i < projectArray.length(); i++) {
+//				JSONObject proj = projectArray.getJSONObject(i);
+//				System.out.println("Key:"+proj.getString("key")+", Name:"+proj.getString("name"));
+//			}
+			
+			//Create Issue
+			String createIssueData = getJSonBody(summary, description, issueType);
+			String issue = invokePostMethod(auth, BASE_URL+"/rest/api/2/issue", createIssueData);
+			System.out.println(issue);
+			JSONObject issueObj = new JSONObject(issue);
+			String newKey = issueObj.getString("key");
+			System.out.println("Key:"+newKey);
+			
+//			//Update Issue
+//			String editIssueData = "{\"fields\":{\"assignee\":{\"name\":\"test\"}}}";
+//			invokePutMethod(auth, BASE_URL+"/rest/api/2/issue/"+newKey, editIssueData);
+			
+			//invokeDeleteMethod(auth, BASE_URL+"/rest/api/2/issue/DEMO-13");
+			
+		} catch (AuthenticationException e) {
+			System.out.println("Username or Password wrong!");
+			e.printStackTrace();
+		} catch (ClientHandlerException e) {
+			System.out.println("Error invoking REST method");
+			e.printStackTrace();
+		} catch (JSONException e) {
+			System.out.println("Invalid JSON output");
+			e.printStackTrace();
+		}
+
+	}
+
+	private static String invokeGetMethod(String auth, String url) throws AuthenticationException, ClientHandlerException {
+		Client client = Client.create();
+		WebResource webResource = client.resource(url);
+		ClientResponse response = webResource.header("Authorization", "Basic " + auth).type("application/json")
+				.accept("application/json").get(ClientResponse.class);
+		int statusCode = response.getStatus();
+		if (statusCode == 401) {
+			throw new AuthenticationException("Invalid Username or Password");
+		}
+		return response.getEntity(String.class);
+	}
+	
+	private static String invokePostMethod(String auth, String url, String data) throws AuthenticationException, ClientHandlerException {
+		Client client = Client.create();
+		WebResource webResource = client.resource(url);
+		ClientResponse response = webResource.header("Authorization", "Basic " + auth).type("application/json")
+				.accept("application/json").post(ClientResponse.class, data);
+		int statusCode = response.getStatus();
+		if (statusCode == 401) {
+			throw new AuthenticationException("Invalid Username or Password");
+		}
+		return response.getEntity(String.class);
+	}
+	
+	private static void invokePutMethod(String auth, String url, String data) throws AuthenticationException, ClientHandlerException {
+		Client client = Client.create();
+		WebResource webResource = client.resource(url);
+		ClientResponse response = webResource.header("Authorization", "Basic " + auth).type("application/json")
+				.accept("application/json").put(ClientResponse.class, data);
+		int statusCode = response.getStatus();
+		if (statusCode == 401) {
+			throw new AuthenticationException("Invalid Username or Password");
+		}
+	}
+	
+	private static void invokeDeleteMethod(String auth, String url) throws AuthenticationException, ClientHandlerException {
+		Client client = Client.create();
+		WebResource webResource = client.resource(url);
+		ClientResponse response = webResource.header("Authorization", "Basic " + auth).type("application/json")
+				.accept("application/json").delete(ClientResponse.class);
+		int statusCode = response.getStatus();
+		if (statusCode == 401) {
+			throw new AuthenticationException("Invalid Username or Password");
+		}
+	}
+	
+	
+    private static String getJSonBody(String summary, String description, String issueType){
+  
+    	
+     	
+        JsonObject createIssueJira
+        = Json.createObjectBuilder()
+                .add("fields",Json.createObjectBuilder()
+        		.add("project",Json.createObjectBuilder().add("key","HACK"))
+                .add("summary", summary)
+                .add("description", description)
+                .add("issuetype", Json.createObjectBuilder().add("name", issueType))
+                ).build();
+ 
+        return createIssueJira.toString();
+ 
+    }
+
+}
